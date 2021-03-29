a8c7512ef919ef602c1619edd6e1eecfedbd7ce5
Ronny Krammer <rkr@knowis.com>
2021-03-29 15:00:52 +0200

    update message

--- src-impl/api/v1/operations/updateUserMessage.ts
+++ src-impl/api/v1/operations/updateUserMessage.ts
@@ -4,6 +4,22 @@ export default class extends operations.v1_updateUserMessage {
   public async execute(): Promise<void> {
     const log = this.util.log;
     log.debug('v1_updateUserMessage.execute()');
+
+    const id = this.request.path.id;
+    const user = this.request.path.user;
+    const { sender, text } = this.request.body;
+    const changeMessageInput = this.factory.entity.mes.ChangeMessage_Input();
+    changeMessageInput.id = id;
+    changeMessageInput.user = user;
+    changeMessageInput.sender = sender;
+    changeMessageInput.text = text.map((content) => {
+      const messageContent = this.factory.entity.mes.MessageContent();
+      messageContent.content = content;
+      return messageContent;
+    });
+
+    void await this.factory.entity.mes.Message().ChangeMessage(changeMessageInput);
+    this.response.status = 200;
   }
 
   /**
